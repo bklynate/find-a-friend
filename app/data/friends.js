1a317815284e8481c78ef284ef265334b469a5cd
bklynate <nathanielcfa@gmail.com>
2017-03-07 12:38:57 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,6 @@
+var friends = [
+{name: "Johnny Depp", gender: "Male", scores: [1,4,5,3,3,1,2,3,1,5], pic: "http://photos.imageevent.com/afap/wallpapers/stars/johnnydepp/huge/johnny-depp---%20-%20Edward%20Scissorhands.jpg"},
+{name: "Jennifer Lawrence", gender: "Female", scores: [2,3,4,5,2,1,2,3,2,1], pic: "http://pixel.nymag.com/imgs/daily/vulture/2015/12/29/29-jennifer-lawrence.w529.h529.jpg"}
+];
+
+module.exports = friends;
